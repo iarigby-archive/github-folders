617f152243fa22d393c493d078cd6ba2db27f976
pouyapanahandeh <panahandehpooya@gmail.com>
2019-05-16 15:24:01 +0200

    api finished

--- api.py
+++ api.py
@@ -2,34 +2,35 @@ from flask import Flask
 
 # api/:user/repos/
 @app.route('/api/<user>/repos')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
+def show_user_repos(user):
+    #returns the list ordered by create date
+    return 'User %s' % user
 
 
 # api/:user/repos/location/
 @app.route('api/<user>/repos/location/')
-def show_user_profile(location):
-    # show the user profile for that user
-    return 'User %s' % username
+def show_user_repos_by_location(user):
+    #returns the list of all repos sorted by location.
+    return 'User %s' % user
 
 
 # api/:user/repos/location/:location
 @app.route('api/<user>/repos/location/<location>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
+def show_user_repos_for_location(user, location):
+    #returns the list of all repos which have a location tag "location"
+    return 'User %s' % user
 
 # api/:user/repos/category
 @app.route('api/<user>/repos/category')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
+def show_user_repos_by_category(user):
+    #returns the list of all repos sorted by category
+    # for example personal projects, school projects, etc
+    return 'User %s' % user
 
 
 
 # api/:user/repos/category/:category
 @app.route('api/<user>/repos/category/<category>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
+def show_user_repos_for_category(user, category):
+    # returns the list of all repos which have a category tag "category"
+    return 'User %s' % user
