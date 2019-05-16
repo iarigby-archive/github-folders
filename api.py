da3ad2fa11653da58cf490c66b72d140ed77f0d3
iarigby <marvinzem@gmail.com>
2019-05-16 17:03:08 +0200

    fixed startup mistakes

--- api.py
+++ api.py
@@ -1,4 +1,9 @@
 from flask import Flask
+app = Flask(__name__)
+
+@app.route('/api/healthCheck')
+def health_check():
+    return 'OK'
 
 # api/:user/repos/
 @app.route('/api/<user>/repos')
@@ -8,29 +13,27 @@ def show_user_repos(user):
 
 
 # api/:user/repos/location/
-@app.route('api/<user>/repos/location/')
+@app.route('/api/<user>/repos/location/')
 def show_user_repos_by_location(user):
     #returns the list of all repos sorted by location.
     return 'User %s' % user
 
 
 # api/:user/repos/location/:location
-@app.route('api/<user>/repos/location/<location>')
+@app.route('/api/<user>/repos/location/<location>')
 def show_user_repos_for_location(user, location):
     #returns the list of all repos which have a location tag "location"
     return 'User %s' % user
 
 # api/:user/repos/category
-@app.route('api/<user>/repos/category')
+@app.route('/api/<user>/repos/category')
 def show_user_repos_by_category(user):
     #returns the list of all repos sorted by category
     # for example personal projects, school projects, etc
     return 'User %s' % user
 
-
-
 # api/:user/repos/category/:category
-@app.route('api/<user>/repos/category/<category>')
+@app.route('/api/<user>/repos/category/<category>')
 def show_user_repos_for_category(user, category):
     # returns the list of all repos which have a category tag "category"
     return 'User %s' % user
