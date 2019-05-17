9bfd2ac1eb2b088ea34509e6355a830aa88547d8
iarigby <marvinzem@gmail.com>
2019-05-17 19:14:15 +0200

    implement parsing

--- github_api.py
+++ github_api.py
@@ -5,8 +5,8 @@ import cache
 github_api = 'https://api.github.com'
 headers = {'Accept': 'application/vnd.github.mercy-preview+json'}
 
-default_locations = ['no_location', 'all']
-default_categories = ['uncategorized']
+default_locations = ['untagged', 'all']
+default_categories = ['untagged']
 
 def get_github_link(repo_name, user):
     return f'{github_api}/repos/{user}/{repo_name}'
@@ -19,20 +19,42 @@ print(category_regex)
 
 def match_github_description(reply_json, regex):
     #return regex.search(reply_json['description'])
-    return regex.search(reply_json['description']).group(1)
+    description = reply_json['description']
+    if description:
+        tag = regex.search(reply_json['description'])
+        if tag:
+            return tag.group(1)
+        else:
+            return 'none'
+    else: 
+        return 'none'
+
+def get_repo_info(repo):
+    return {
+        'name': repo['name'],
+        'description': repo['description'],
+        'url': repo['url']
+}
 
 def get_user_repos(user):
     # repos = requests.get(f'https://api.github.com/users/{user}/repos', headers=headers).json()
     repos = cache.get()
     # return list(map(lambda x: x['description'], repos))
-    return parse_descriptions(repos)
-    
+    # return parse_descriptions(repos)
+    return list(map(lambda x: get_repo_info(x), repos))
     # for j in r.json():
       #   repo_name = j["name"]
        #  req = requests.get(f"{get_github_link(repo_name)}/topics", headers=headers)
 
-def parse_descriptions(repos):
-    locations = {}
-    categories = {}
-    return list(map(lambda x: match_github_description(x, location_regex), repos))
-    
+# tag type can be location or cateogry
+def parse_descriptions(repos, regex_type):
+    categorized_repos = {}
+    for repo in repos:
+        tag = match_github_description(repo, regex_type)
+        if tag in categorized_repos:
+            categorized_repos[tag].append(repo)
+        else:
+            categorized_repos[tag] = [repo]
+    #return list(map(lambda x: match_github_description(x, regex_type), repos))
+    return categorized_repos
+# compdef 
