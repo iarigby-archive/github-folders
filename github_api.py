4f3c5a1c13887d9fa4b9b7d472151d6a3b82a1e1
pooya <pouyapanahandeh@users.noreply.github.com>
2019-05-17 14:57:01 +0200

    find location

--- github_api.py
+++ github_api.py
@@ -11,9 +11,8 @@ default_categories = ['uncategorized']
 def get_github_link(repo_name, user):
     return f'{github_api}/repos/{user}/{repo_name}'
 
+# re.search(r'([a-zA-Z0-9-]*)', get_github_link).group()
 location_regex = re.compile(r'location:([a-zA-Z0-9-]*)')
-#result = location_regex.match("home")
-#print(result)
 category_regex = re.compile(r'category:([a-zA-Z0-9-]*)')
 
 print(category_regex)
